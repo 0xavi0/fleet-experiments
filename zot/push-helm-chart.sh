b815d4b3399f5bca5d250b37fb0d6c6fd58ad1f7
Xavi Garcia <xavi.garcia@suse.com>
2025-08-07 11:48:01 +0200

    Update push-helm-chart.sh

--- zot/push-helm-chart.sh
+++ zot/push-helm-chart.sh
@@ -6,5 +6,5 @@ then
   exit 1
 fi
 helm registry login -u fleet-ci ${1}:8082 --insecure
-helm push guestbook-0.0.1.tgz oci://${1}:8082/guestbook --insecure-skip-tls-verify
+helm push guestbook-0.0.1.tgz oci://${1}:8082 --insecure-skip-tls-verify
 helm registry logout ${1}:8082
